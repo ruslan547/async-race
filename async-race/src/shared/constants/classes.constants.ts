7dbf3caa9e57314a948f48999e37e0cd34b4ee06
ruslan547 <novik.rv93@gmail.com>
2021-06-09 09:16:20 +0300

    feat: add color setting

--- async-race/src/shared/constants/classes.constants.ts
+++ async-race/src/shared/constants/classes.constants.ts
@@ -9,4 +9,7 @@ export enum ClassesConstants {
   MAIN_BUTTON = 'main-button',
   NAV = 'nav',
   PAGE_NUMBER = 'page-number',
+  COLOR_SETTING = 'color-setting',
+  COLOR_EXAMPLE = 'color-example',
+  COLOR_INPUT = 'color-input',
 }
